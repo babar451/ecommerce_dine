1745fcb6841acce1b4d541ec83507c768e53ee5d
Babar Ali <babralijafr3@gmail.com>
2024-03-05 16:35:46 +0500

    dine marketplace

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,10 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { sora } from "./fonts";
 import "./globals.css";
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
 
-const inter = Inter({ subsets: ["latin"] });
+
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,7 +18,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={sora.className}>
+        <Header/>
+        <main className="px-6 mx-3 lg:px-16 lg:mx-8">
+        {children}
+        </main>
+        <Footer />
+        </body>
     </html>
   );
 }
